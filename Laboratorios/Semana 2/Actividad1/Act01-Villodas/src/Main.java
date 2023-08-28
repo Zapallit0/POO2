09b40d7b609859a9658dab3a226c30ac092d22c9
Jhersvin Villodas Quinto <jhersvinvillodasquinto@gmail.com>
2023-08-27 23:54:23 -0500

    Ultima parte sin completar superficieplana

--- Laboratorios/Semana 2/Actividad1/Act01-Villodas/src/Main.java
+++ Laboratorios/Semana 2/Actividad1/Act01-Villodas/src/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Triangulo triangulo = new Triangulo("Triángulo", new Punto(0, 0), new Punto(4, 0), new Punto(2, 3));
         System.out.println("Área de " + triangulo.getNombre() + ": " + triangulo.calcularArea());
+
+        Triangulo robetocarlos=new Triangulo("triangulo",new Punto(0,0),new Punto(6,0),new Punto(3,5));
+        System.out.println(robetocarlos.esRegular());
+
+        Punto xd=new Punto(0,0);
+        Punto xd2=new Punto(6,0);
+        System.out.println(xd.calcularDistancia(xd2));
+
     }
 }
